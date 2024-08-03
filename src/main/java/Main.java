1f9e944bb8bf5608b9ef4c1ea71eed991495d354
Eugeny Kalemenev <e.kalemenev@yadro.com>
2024-08-03 23:26:58 +0300

    Main: add user input to main method
    This adds user data input to fill cars list participating in the
    race and get the winner of the race.
    
    Signed-off-by: Eugeny Kalemenev <e.kalemenev@yadro.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static final int CAR_MIN_SPEED_KMH = 0;
+    public static final int CAR_MAX_SPEED_KMH = 250;
+    public static final int MAX_CARS_IN_RACE = 3;
+    public static String resultMessageTemplate = "Самая быстрая машина: %s.";
+    public static String addNewCarMessageTemplate = "Введите данные автомобиля N%d";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> carsInRace = new ArrayList<>();
+
+        System.out.println("Введите данные автомобилей учавствующих в гонке.");
+        for(int i = 0; i < MAX_CARS_IN_RACE; i++) {
+            System.out.println(String.format(addNewCarMessageTemplate, i + 1));
+            System.out.print("Имя: ");
+            String carName = scanner.next();
+            int carSpeed;
+            while (true) {
+                System.out.print("Скорость (км/ч): ");
+                carSpeed = scanner.nextInt();
+                if (carSpeed < CAR_MIN_SPEED_KMH) {
+                    System.out.println("Скорость должна быть больше или равна " + CAR_MIN_SPEED_KMH);
+                } else if (carSpeed > CAR_MAX_SPEED_KMH) {
+                    System.out.println("Скорость должна быть меньше или равна " + CAR_MAX_SPEED_KMH);
+                } else {
+                    break;
+                }
+            }
+            carsInRace.add(new Car(carName,carSpeed));
+        }
+        Race race = new Race(carsInRace);
+        Car carWinner = race.getRaceWinner();
+        if (carWinner != null) {
+            System.out.println(String.format(resultMessageTemplate, carWinner.name));
+        } else {
+            System.out.println("Fail. Something is going wrong.");
+        }
     }
 }
